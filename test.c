ab0c3bbd9f1cc58a23ef855f2e7cb8291b2d28b2
Roman Mazalov <imspeedwagon@gmail.com>
2021-04-14 14:34:38 +0300

    Add test.c

---
+++ test.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdint.h>
+
+// Main function
+int32_t main(void) {
+    // Test comment
+    int a = 1;
+    if (a <= 1)
+    	printf("Test World!\n");
+    else
+	print("a > 1"):
+    return 1;
+}
